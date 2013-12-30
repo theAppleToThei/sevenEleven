adf4814943d0a56859f470ec953c2515d57f5f2c
Alex Baratti <alex.techtalk4geeks@gmail.com>
2013-12-29 16:17:53 -0800

    - Added Stealers - Formated Receipts

--- sevenEleven/src/SevenEleven.java
+++ sevenEleven/src/SevenEleven.java
@@ -9,9 +9,27 @@ public class SevenEleven {
 	ArrayList<HotDog> rotisserie = new ArrayList<HotDog>();
 	
 	Customer joe = new Customer("Joe", 10, "Starberry Hot Chocolate");
+	Customer mark = new Customer("Mark", 30, "Coke");
+	Customer rob = new Customer("Rob", 0, "Revenge");
 
 	public SevenEleven() {
 		customers.add(joe);
+		stockStore();
+		
+		fillCup(joe, cups.get(new Random().nextInt(cups.size())));
+		hotDogGet(joe, rotisserie.get(new Random().nextInt(rotisserie.size())));
+		checkOut(joe);
+		
+		customers.add(mark);
+		hotDogGet(mark, rotisserie.get(new Random().nextInt(rotisserie.size())));
+		checkOut(mark);
+		
+		customers.add(rob);
+		hotDogGet(rob, rotisserie.get(new Random().nextInt(rotisserie.size())));
+		steal(rob);
+	}
+
+	private void stockStore() {
 		cups.add(new Cup("Big Gulp", 1.79, 64));
 		cups.add(new Cup("Big Gulp", 1.79, 64));
 		cups.add(new Cup("Big Gulp", 1.79, 64));
@@ -50,9 +68,7 @@ public class SevenEleven {
 		rotisserie.add(new HotDog("Big Bite", 2.99));
 		rotisserie.add(new HotDog("Big Bite", 2.99));
 		
-		fillCup(joe, cups.get(new Random().nextInt(cups.size())));
-		hotDogGet(joe, rotisserie.get(new Random().nextInt(rotisserie.size())));
-		checkOut(joe);
+		//Coming soon: Lotto tickets
 	}
 	
 	public static void main(String[] args) {
@@ -78,12 +94,22 @@ public class SevenEleven {
 //			returnItems();
 		}
 
-System.out.println("7/eleven\n");
-		for (int j = 0; j < c.getCart().size(); j++) {
-			System.out.println(c.getCartItem(j).getName() + "          \n$" + c.getCartItem(j).getPrice());
-		}
-		System.out.println("Your total today is: $" + c.getCartPrice());
-		System.out.println("Thank you! Have a nice day!");
+		printReceipt(c);
+	}
+	
+	public void steal(Customer c) {
+		System.out.println("--STOLEN ITEM DETECTED--\n--ACTIVATING ALARM--");
+		customers.remove(c);
+	}
+
+	private void printReceipt(Customer c) {
+		System.out.println("7/Eleven\n");
+		System.out.println("Customer: " + c.getName());
+				for (int j = 0; j < c.getCart().size(); j++) {
+					System.out.println(c.getCartItem(j).getName() + "          \n$" + c.getCartItem(j).getPrice());
+				}
+				System.out.println("Your total today is: $" + c.getCartPrice());
+				System.out.println("Thank you! Have a nice day!\n\n----------------------\n");
 	}
 
 }
